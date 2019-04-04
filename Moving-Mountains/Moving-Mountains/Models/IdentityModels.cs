9eeca1fe955073c65b5e360036513687553ecff9
kcraych@gmail.com <kcraych@gmail.com>
2019-04-04 16:33:40 -0600

    Add Context connection string

--- Moving-Mountains/Moving-Mountains/Models/IdentityModels.cs
+++ Moving-Mountains/Moving-Mountains/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace Moving_Mountains.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("MountainsContext", throwIfV1Schema: false)
         {
         }
 
